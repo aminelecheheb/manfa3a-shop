16f37d12dfee49e7a9b3b374f83307bad7432bc8
aminelecheheb <aminelch01@gmail.com>
2023-10-25 19:56:20 +0100

    navbar desktop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import "@/styles/globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "Manfa3a Shop",
@@ -15,8 +13,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ar">
+      <body className="wrapper">
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import styles from "@/styles/Layout.module.css";
+import Link from "next/link";
+import { FaShopify } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <div className="container">
+        <div className={styles.nav_flex}>
+          <div className={styles.logo}>
+            <div>
+              <h1 className={styles.logo}>منفعة شوب</h1>
+              <h2>Weal Shop</h2>
+            </div>
+            <FaShopify className={styles.icon} />
+          </div>
+
+          <div className={styles.links}>
+            <Link href="/">
+              <h3>الصفحة الرئيسية</h3>
+            </Link>
+            <Link href="/shop">
+              <h3>جميع الفئات</h3>
+            </Link>
+            <Link href="/feedbacks">
+              <h3>آراء الزبائن</h3>
+            </Link>
+          </div>
+          <div className={styles.infos}>
+            <div>
+              <h3>اتصل بنا</h3>
+              <p>07 76 52 12 25</p>
+            </div>
+            <div>
+              <h3>توصيل متوفر</h3>
+              <p> 58 ولاية</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
