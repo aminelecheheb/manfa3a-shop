b266f43d120208b6e6150f7b81b5f4ea5de74622
aminelecheheb <aminelch01@gmail.com>
2023-11-11 08:18:29 +0100

    style logOut btn

--- src/components/auth.tsx
+++ src/components/auth.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { signIn, signOut } from "next-auth/react";
+import styles from "@/styles/Dashboard.module.css";
+import { AiOutlinePoweroff } from "react-icons/ai";
 
 export const LoginButton = () => {
   return <button onClick={() => signIn()}>Sign in</button>;
 };
 
 export const LogoutButton = () => {
-  return <button onClick={() => signOut()}>Sign Out</button>;
+  return (
+    <button className={styles.signout_btn} onClick={() => signOut()}>
+      <AiOutlinePoweroff className={styles.signout_icon} />
+      <h3>تسجيل الخروج</h3>
+    </button>
+  );
 };
