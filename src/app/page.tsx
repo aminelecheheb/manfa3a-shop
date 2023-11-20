a84a5261162048bb16250e5484185f221b99d6fc
aminelecheheb <aminelch01@gmail.com>
2023-11-20 08:13:36 +0100

    home slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Slider from "@/components/swiper/Slider";
 import styles from "@/styles/page.module.css";
 import LivraisonImages from "@/components/LivraisonImages";
+import { getAllProducts } from "@/lib/product";
+import HomeSlider from "@/components/HomeSlider";
 
-export default function Home() {
+export default async function Home() {
+  const { products } = await getAllProducts(true);
   return (
     <main>
       <Slider />
+      {products && <HomeSlider products={products} />}
       <section className={styles.livraison_section}>
         <div className={styles.livraison_container}>
           <LivraisonImages />
---
+++ src/components/HomeSlider.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import React, { useRef, useState, useEffect } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+import styles from "@/styles/page.module.css";
+
+// import required modules
+import { FreeMode, Pagination } from "swiper/modules";
+import Product from "./Product";
+
+const HomeSlider = ({ products }: { products: ProductProps[] }) => {
+  const [x, setX] = useState(400);
+  const swiperRef = useRef(null);
+
+  useEffect(() => {
+    window.innerWidth > 500
+      ? setX(window.innerWidth / 400)
+      : setX(window.innerWidth / 300);
+  }, []);
+
+  const checkX = () => {
+    window.innerWidth > 500
+      ? setX(window.innerWidth / 400)
+      : setX(window.innerWidth / 300);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", checkX);
+    return () => {
+      window.removeEventListener("resize", checkX);
+    };
+  });
+  return (
+    <div className={styles.my_wrapper}>
+      <div className="container">
+        <h2 className={styles.slider_title}>افضل المنتجات</h2>
+        <Swiper
+          slidesPerView={x}
+          spaceBetween={30}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          className="mySwiper"
+        >
+          {products.map((product) => {
+            return (
+              <SwiperSlide>
+                <Product product={product} />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSlider;
